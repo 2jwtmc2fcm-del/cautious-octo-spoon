96107a5d556c5e3104b3ddabc5f2526687b51c59
2jwtmc2fcm-del <2jwtmc2fcm@privaterelay.appleid.com>
2026-04-25 13:21:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Bot crypto attivo 🚀")
+import requests
+
+price = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur").json()
+
+print("Prezzo BTC:", price["bitcoin"]["eur"], "€")
